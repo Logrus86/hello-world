544821f477ddb08f19becc65d5ae13a949e6aca4
Logrus86 <logr86@gmail.com>
2015-01-19 09:38:40 +0600

    test

--- src/main/java/com/epam/bp/helloworld/Runner.java
+++ src/main/java/com/epam/bp/helloworld/Runner.java
@@ -2,6 +2,6 @@ package com.epam.bp.helloworld;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Hello World! version 3");
+        System.out.println("Hello World! version branch 2");
     }
 }
