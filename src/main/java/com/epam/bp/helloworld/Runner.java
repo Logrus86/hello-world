972aa355aad8ad3a2db02e80ce3c1f3c48d01181
Logrus86 <logr86@gmail.com>
2015-01-19 09:19:13 +0600

    super branch+

--- src/main/java/com/epam/bp/helloworld/Runner.java
+++ src/main/java/com/epam/bp/helloworld/Runner.java
@@ -2,6 +2,6 @@ package com.epam.bp.helloworld;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Hello World! version 2");
+        System.out.println("SuperHello World! version 0");
     }
 }
